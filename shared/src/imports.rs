34128670943b9b4ec6bb7687a98a76fb5bf6bdfd
xshady <fastik036@gmail.com>
2025-02-07 20:57:17 +0300

    remove eprintln

--- shared/src/imports.rs
+++ shared/src/imports.rs
@@ -5,7 +5,4 @@ pub trait ___Internal___Imports___ {
   fn on_alloc(module: ModuleId, ptr: *mut u8, layout: StableLayout);
   fn on_cached_allocs(module: ModuleId, ops: SliceAllocatorOp);
   fn unrecoverable(message: Str) -> !;
-
-  // for avoiding allocations & thread local creation inside the module
-  fn eprintln(message: Str);
 }
