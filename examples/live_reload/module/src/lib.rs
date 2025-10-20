80f6075caf9fdb853d24d0f4521a775fce26223c
xshady <fastik036@gmail.com>
2025-10-20 22:42:38 +0300

    restore live_reload wip

--- examples/live_reload/module/src/lib.rs
+++ examples/live_reload/module/src/lib.rs
@@ -1,8 +1,4 @@
 #[relib_module::export]
 fn main() {
-  let time = std::time::SystemTime::now();
-  std::thread::sleep(std::time::Duration::from_secs(1));
-
-  let time2 = std::time::SystemTime::now();
-  println!("diff: {:?}", time2 - time);
+  println!("change me!");
 }
